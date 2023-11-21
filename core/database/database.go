5e1e24a412b0fee6361dc3fc2eb74443d405ac13
JngMkk <kjm0817ss@gmail.com>
2023-11-21 15:50:10 +0900

    feat: db connection

---
+++ core/database/database.go
@@ -0,0 +1,33 @@
+package database
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/JngMkk/golang-fiber/apps/models"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func Connect() *gorm.DB {
+	dsn := "root:1234@tcp(127.0.0.1:3306)/test?charset=utf8mb4&parseTime=true"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println("DB connection Error: ", err)
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		fmt.Println("DB connection Error: ", err)
+	}
+
+	sqlDB.SetMaxIdleConns(5)
+	sqlDB.SetMaxOpenConns(50)
+	sqlDB.SetConnMaxLifetime(10 * time.Minute)
+
+	return db
+}
+
+func AutoMigrate(db *gorm.DB) {
+	db.AutoMigrate(&models.User{})
+}
