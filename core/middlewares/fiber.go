5c9e4e3593da3732aa154e3eab7575aaaccae266
JngMkk <kjm0817ss@gmail.com>
2023-11-21 15:50:45 +0900

    feat: fiber middlware config

---
+++ core/middlewares/fiber.go
@@ -0,0 +1,18 @@
+package middlewares
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func FiberMiddleWare(a *fiber.App) {
+	a.Use(
+		cors.New(cors.Config{
+			AllowOrigins: "*",
+			AllowHeaders: "Origin, Content-Type, Accept, Authorization",
+			AllowMethods: "GET, HEAD, PUT, PATCH, POST, DELETE",
+		}),
+		logger.New(),
+	)
+}
